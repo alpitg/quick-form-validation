446ab0f93e89dcdc11db247d43663e3394741c50
alpit-g <alpit.gajbhiye@smartdatainc.net>
2019-02-04 17:19:05 +0530

    Form validations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'formvalidation';
+
+  form: FormGroup;
+  submitted = false;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.form = formBuilder.group({
+      firstName: ['', Validators.required],
+      age: ['', Validators.required]
+    });
+  }
+
+  hasFieldError(fieldName: string): boolean {
+    return this.form.controls[fieldName].invalid && (this.submitted || this.form.controls[fieldName].touched);
+  }
 }
